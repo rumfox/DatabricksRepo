f63872fe692acba41a043b0246c3dbc9894fc474
rumfox <rumfox>
2023-06-16 02:11:11 +0000

    PySpark Study 2nd day.

--- Example/[Git]Notebook 2023-06-15 15_42_58.py
+++ Example/[Git]Notebook 2023-06-15 15_42_58.py
@@ -7,3 +7,36 @@
 
 # MAGIC %sql
 # MAGIC SELECT "GIT Repository Test"
+
+# COMMAND ----------
+
+from pyspark.sql import SparkSession
+
+# Create a SparkSession
+spark = SparkSession.builder.getOrCreate()
+
+# Sample data
+data = [
+    {"name": "John", "age": 25, "city": "New York"},
+    {"name": "Alice", "age": 30, "city": "London"},
+    {"name": "Bob", "age": 35, "city": "Paris"}
+]
+
+# Create DataFrame
+df = spark.createDataFrame(data)
+
+# Show DataFrame
+df.show()
+
+
+# COMMAND ----------
+
+from pyspark.sql.functions import substring;
+from pyspark.sql.functions import col
+
+# df.select(substring(col("string"),start, length))
+
+# Extract substring from 'name' column
+df = df.withColumn("substring", col("name").substr(1, 4))
+
+df.show()
